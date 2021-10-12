e879e886ede7a180fd79d9affffa224d4a073f85
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-10-12 14:16:48 -0300

    Fix db connection

--- config/install.php
+++ config/install.php
@@ -2,7 +2,7 @@
 try 
 {
   $dbclass = new DBClass(); 
-  $connection = $dbclass.getConnection();
+  $connection = $dbclass->getConnection();
   $sql = file_get_contents("database/database.sql"); 
 echo $sql;
   $connection->exec($sql);
