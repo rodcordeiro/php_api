568e75e9baa900f1d0e1e8d0965826002e6c944f
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-12-09 11:30:24 -0300

    Update db.class.php

--- db/db.class.php
+++ db/db.class.php
@@ -1,10 +1,6 @@
 <?php
 class DBClass {
 
-    private $host = "localhost";
-    private $username = "id8060145_cordeiro";
-    private $password = "@Drigo13";
-    private $database = "id8060145_conceptive";
 
     public $connection;
 
